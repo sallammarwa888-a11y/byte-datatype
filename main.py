eaeee2ef4d5f08e5cfcdb40f2775e800fd63010b
sallammarwa888-a11y <sallammarwa888@gmail.com>
2026-07-06 01:18:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-"""Compatibility module for direct file usage."""
-from . import Tryte
-
-if __name__ == "__main__":
-    #example:
-    # a = Tryte(26)
-    # print(a)
-    # print(a + 1)
-    ...
+#..?
